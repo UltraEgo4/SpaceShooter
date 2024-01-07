99471d68d68697c177ccf7059596803d5775e2ce
UltraEgo4 <ujfalus.biz@gmail.com>
2024-01-07 23:23:14 +0100

    entity base class created

--- include/entity.h
+++ include/entity.h
@@ -1,3 +1,24 @@
 #ifndef ENTITY_H
 #define ENTITY_H
+
+#include <SFML/System/Vector2.hpp>
+
+
+class Entity{
+public:
+
+    Entity();
+
+    void setVelocity(sf::Vector2f velocity);
+    void setVelocity(float vx, float vy);
+
+    sf::Vector2f getVelocity(sf::Vector2f velocity) const;
+
+private:
+    sf::Vector2f mVelocity;
+
+
+};
+
+
 #endif
